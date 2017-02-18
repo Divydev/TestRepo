cf24cffc50412a71083d4ceb47d712a2d88ccae8
divy Aggarwal <divy Aggarwal>
2017-02-18 14:10:07 -0600

    testing

--- Hello.java
+++ Hello.java
@@ -1,8 +1,12 @@
 public class Hello {
 	public static void main(String[] args){
-		for(int i =0; i<5; i++){
-			System.out.println("In loop " + i);
+		int i = 1;
+		for(;i<=5; i++){
+			System.out.println("In loop 1 for i = " + i);
 		}
-			
+		
+	for(;i<=10; ++i){
+			System.out.println("In loop 2 for i = " + i);
+		}		
 	}
 }
